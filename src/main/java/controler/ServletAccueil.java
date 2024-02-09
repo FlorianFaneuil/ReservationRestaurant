b52c0e0d7a74dadc5f9de2957064661cb79daaff
Florian <florian.faneuil@gmail.com>
2024-02-09 10:17:58 +0100

    vendredi 09

--- src/main/java/controler/ServletAccueil.java
+++ src/main/java/controler/ServletAccueil.java
@@ -1,6 +1,9 @@
 package controler;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
 
 import bll.BLLException;
@@ -15,6 +18,8 @@ public class ServletAccueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private RestaurantBLL restaurantBll;
 	private List<Restaurant> restaurants = null;
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH'h'mm");
 
 	@Override
 	public void init() throws ServletException {
@@ -34,7 +39,24 @@ public class ServletAccueil extends HttpServlet {
 		} catch (BLLException e) {
 			e.printStackTrace();
 		}
+		
+		//Passage de l'heure d'ouverture formatée dans une HashMap pour pouvoir la récupérer dans la page d'accueil
+		HashMap<Integer, String> restaurantOuverture = new HashMap<>();
+		for (Restaurant restaurant : restaurants) {
+			String heureOuvertureFormat = restaurant.getHeureOuverture().format(formatter);
+			restaurantOuverture.put(restaurant.getId(), heureOuvertureFormat);
+		}
+		
+		//Passage de l'heure de fermeture formatée dans une HashMap pour pouvoir la récupérer dans la page d'accueil
+		HashMap<Integer, String> restaurantFermeture = new HashMap<>();
+		for (Restaurant restaurant : restaurants) {
+			String heureOuvertureFormat = restaurant.getHeureFermeture().format(formatter);
+			restaurantFermeture.put(restaurant.getId(), heureOuvertureFormat);
+		}
+		
 		request.setAttribute("restaurants", restaurants);
+		request.setAttribute("restaurantOuverture", restaurantOuverture);
+		request.setAttribute("restaurantFermeture", restaurantFermeture);
 		request.getRequestDispatcher("/WEB-INF/jsp/nonConnecte/accueil.jsp").forward(request, response);
 	}
 
