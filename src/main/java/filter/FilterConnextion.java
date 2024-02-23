2491d65e9b0178896d35edc5ae8aff9ae86f101d
Melien Pierre <pimerors@gmail.com>
2024-02-23 14:41:29 +0100

    mise en place filtre de onnexion

--- src/main/java/filter/FilterConnextion.java
+++ src/main/java/filter/FilterConnextion.java
@@ -1,27 +1,39 @@
 package filter;
 
+import java.io.IOException;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
-import java.io.IOException;
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class FilterConnextion extends HttpFilter implements Filter {
-  
-	
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-	  System.out.println("passage dans le filtre");
-		chain.doFilter(request, response);
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		Boolean estConnecte = (Boolean) req.getSession().getAttribute("estConnecte");
+
+		if (estConnecte != null && estConnecte) {
+			
+			chain.doFilter(request, response);
+		} else {
+			
+			res.sendRedirect(req.getContextPath() + "/connexion");
+		}
 	}
 
-	public void init(FilterConfig fConfig) throws ServletException {
-		
+	public void destroy() {
+		// Destruction du filtre
 	}
 
 }
